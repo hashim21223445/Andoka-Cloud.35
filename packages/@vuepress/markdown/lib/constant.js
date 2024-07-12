b643d987306764e06aa2376ba14263b65fab0a0d
‪Hashim Mohammad‬‏ <155690197+hashim21223445@users.noreply.github.com>
2024-07-12 11:37:37 +0200

    Initialize jess/img-explicit-dimensions

---
+++ packages/@vuepress/markdown/lib/constant.js
@@ -0,0 +1,18 @@
+exports.PLUGINS = {
+  ANCHOR: 'anchor',
+  COMPONENT: 'component',
+  CONVERT_ROUTER_LINK: 'convert-router-link',
+  EMOJI: 'emoji',
+  HIGHLIGHT_LINES: 'highlight-lines',
+  HOIST_SCRIPT_STYLE: 'hoist-script-style',
+  IMG_SIZE: 'img-size',
+  LINE_NUMBERS: 'line-numbers',
+  PRE_WRAPPER: 'pre-wrapper',
+  SNIPPET: 'snippet',
+  TOC: 'toc'
+}
+
+exports.REQUIRED_PLUGINS = [
+  exports.PLUGINS.ANCHOR,
+  exports.PLUGINS.COMPONENT
+]
